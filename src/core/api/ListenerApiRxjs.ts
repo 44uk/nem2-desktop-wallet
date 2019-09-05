30282856ae8bac00612ead565d656f1d93d81267
decentraliser <decentraliser@gmail.com>
2019-09-05 19:15:45 +0800

    move listeners

--- src/core/api/ListenerApiRxjs.ts
+++ src/core/api/ListenerApiRxjs.ts
@@ -1,46 +1,12 @@
 import {filter, mergeMap} from 'rxjs/operators'
 import {Address, Listener, TransactionHttp} from 'nem2-sdk'
 import {from as observableFrom} from "rxjs"
+import {map} from "rxjs/operators"
 
 export class ListenerApiRxjs {
 
     public openWs(listener: any) {
         return observableFrom(listener.open())
-    }
-
-
-    listenerUnconfirmed(listener: any, address: Address, fn: any) {
-        return observableFrom(listener.open().then(() => {
-            listener
-                .unconfirmedAdded(address)
-                .pipe(
-                    filter((transaction: any) => transaction.transactionInfo !== undefined)
-                )
-                .subscribe(transactionInfo => {
-                    fn(transactionInfo)
-                })
-        }))
-    }
-
-    listenerConfirmed(listener: any, address: Address, fn: any) {
-        return observableFrom(listener.open().then(() => {
-            listener.confirmed(address)
-                .pipe(filter((transaction: any) => transaction.transactionInfo !== undefined))
-                .subscribe(transactionInfo => {
-                    fn(transactionInfo)
-                })
-        }))
-    }
-
-    listenerTxStatus(listener: any, address: Address, fn: any) {
-
-        return observableFrom(listener.open().then(() => {
-            listener
-                .status(address)
-                .subscribe(transactionInfo => {
-                    fn(transactionInfo)
-                })
-        }))
     }
 
     sendMultisigWs(address: Address, account: any, node: string, signedBondedTx: any, signedLockTx: any, listener: any) {
@@ -56,25 +22,5 @@ export class ListenerApiRxjs {
                 )
                 .subscribe()
         }))
-    }
-
-
-    newBlock(listener: Listener, callback) {
-        return observableFrom(listener.open().then(() => {
-                listener
-                    .newBlock()
-                    .subscribe(
-                        (block) => {
-                            const chainStatus = {
-                                numTransactions: block.numTransactions ? block.numTransactions : 0,
-                                signerPublicKey: block.signer.publicKey,
-                                currentHeight: block.height.compact(),
-                                currentBlockInfo: block,
-                            }
-                            callback(chainStatus)
-                        },
-                    )
-            })
-        )
     }
 }
--- src/core/services/listeners.ts
+++ src/core/services/listeners.ts
@@ -1,103 +1,145 @@
 import {Address, Listener} from "nem2-sdk";
-import {ListenerApiRxjs} from "@/core/api/ListenerApiRxjs.ts"
+import {filter} from 'rxjs/operators'
 
-export class Listeners {
-  constructor(address: string, node: string, app: any) {
-    this.address = address
-    this.node = node
-    this.app = app
-  }
-  address: string
-  node: string
-  app: any
-  confirmedTxList: any = []
-  unconfirmedTxList: any = []
-  errorTxList: any = []
-  unconfirmedTxListener: any
-  confirmedTxListener: any
-  txStatusListener: any
+export class ChainListeners {
+    private readonly app: any
+    private node: string
+    // @TODO address as Address
+    private address: string
 
-  start() {
-    this.unconfirmedListener()
-    this.confirmedListener()
-    this.txErrorListener()
-  }
-
-  disposeConfirmed(transaction): void {
-      let list = this.confirmedTxList
-      let unList = this.unconfirmedTxList
-      if (!list.includes(transaction.transactionInfo.hash)) {
-          list.push(transaction.transactionInfo.hash)
-          if (unList.includes(transaction.transactionInfo.hash)) {
-              unList.splice(unList.indexOf(transaction.transactionInfo.hash), 1)
-          }
-          this.confirmedTxList = list
-          this.unconfirmedTxList = unList
-          this.app.$Notice.destroy()
-          this.app.$Notice.success({
-              title: this.app.$t('Transaction_Reception').toString(),
-              duration: 4,
-          })
-      }
-  }
-
-  disposeTxStatus(transaction): void {
-      let list = this.errorTxList
-      if (!list.includes(transaction.hash)) {
-          list.push(transaction.hash)
-          this.app.$store.commit('SET_ERROR_TEXT', list)
-          this.app.$Notice.destroy()
-          this.app.$Notice.error({
-              title: transaction.status.split('_').join(' '),
-              duration: 10,
-          })
-      }
-  }
-
-  disposeUnconfirmed(transaction): void {
-      let list = this.unconfirmedTxList
-      if (!list.includes(transaction.transactionInfo.hash)) {
-          list.push(transaction.transactionInfo.hash)
-          this.unconfirmedTxList = list
-          this.app.$Notice.success({
-              title: this.app.$t('Transaction_sending').toString(),
-              duration: 20,
-          })
-      }
-  }
-
-  unconfirmedListener(): void {
-      const node = this.node.replace('http', 'ws')
-      this.unconfirmedTxListener && this.unconfirmedTxListener.close()
-      this.unconfirmedTxListener = new Listener(node, WebSocket)
-      new ListenerApiRxjs().listenerUnconfirmed(this.unconfirmedTxListener, Address.createFromRawAddress(this.address), this.disposeUnconfirmed)
-  }
-
-  confirmedListener(): void {
-      const node = this.node.replace('http', 'ws')
-      this.confirmedTxListener && this.confirmedTxListener.close()
-      this.confirmedTxListener = new Listener(node, WebSocket)
-      new ListenerApiRxjs().listenerConfirmed(this.confirmedTxListener,
-        Address.createFromRawAddress(this.address), this.disposeConfirmed)
-  }
-
-  txErrorListener(): void {
-      const node = this.node.replace('http', 'ws')
-      this.txStatusListener && this.txStatusListener.close()
-      this.txStatusListener = new Listener(node, WebSocket)
-      new ListenerApiRxjs().listenerTxStatus(this.txStatusListener, Address.createFromRawAddress(this.address), this.disposeTxStatus)
-  }
-
-  chainListner() {
-    if (!this.node) {
-        return
+    constructor(app: any, address: string, endpoint: string) {
+        this.app = app
+        this.address = address
+        this.node = endpoint.replace('http', 'ws')
     }
-    const node = this.node.replace('http', 'ws')
-    const listener = new Listener(node, WebSocket)
-    new ListenerApiRxjs().newBlock(listener, this.setChainStatus)
-  }
 
-  setChainStatus(chainStatus) {
-    this.app.$store.commit('SET_CHAIN_STATUS', chainStatus)
-  }
+    confirmedTxList: any = []
+    unconfirmedTxList: any = []
+    errorTxList: any = []
+    unconfirmedTxListener: Listener
+    confirmedTxListener: Listener
+    txStatusListener: Listener
+    newBlocksListener: Listener
+
+    start() {
+        console.log(`starting listeners for ${this.address} on ${this.node}`)
+        this.unconfirmedListener()
+        this.confirmedListener()
+        this.txErrorListener()
+        this.chainListener()
+    }
+    
+    switchAddress(address: string) {
+        this.address = address
+        console.log(`ChainListeners switchAddress to ${this.address}`)
+        this.unconfirmedListener()
+        this.confirmedListener()
+        this.txErrorListener()
+    }
+
+    switchEndpoint(endpoint: string) {
+        this.node = endpoint.replace('http', 'ws')
+        console.log(`ChainListeners switchEndpoint to ${this.node}`)
+        this.chainListener()
+    }
+
+    unconfirmedListener(): void {
+        this.unconfirmedTxListener && this.unconfirmedTxListener.close()
+        const {unconfirmedTxList} = this
+        const notice = this.app.$Notice
+        this.unconfirmedTxListener = new Listener(this.node, WebSocket)
+        this.unconfirmedTxListener
+            .open()
+            .then(() => {
+                this.unconfirmedTxListener
+                    .confirmed(Address.createFromRawAddress(this.address))
+                    .pipe(filter((transaction: any) => transaction.transactionInfo !== undefined))
+                    .subscribe(transaction => {
+                        if (!unconfirmedTxList.includes(transaction.transactionInfo.hash)) {
+                            unconfirmedTxList.push(transaction.transactionInfo.hash)
+                            notice.success({
+                                title: this.app.$t('Transaction_sending').toString(),
+                                duration: 20,
+                            })
+                        }
+                    })
+            })
+
+    }
+
+    
+    confirmedListener(): void {
+        this.confirmedTxListener && this.confirmedTxListener.close()
+        const {confirmedTxList, unconfirmedTxList} = this
+        const notice = this.app.$Notice
+        this.confirmedTxListener = new Listener(this.node, WebSocket)
+        this.confirmedTxListener
+            .open()
+            .then(() => {
+                this.confirmedTxListener
+                    .confirmed(Address.createFromRawAddress(this.address))
+                    .pipe(filter((transaction: any) => transaction.transactionInfo !== undefined))
+                    .subscribe(transaction => {
+                        if (!confirmedTxList.includes(transaction.transactionInfo.hash)) {
+                            confirmedTxList.push(transaction.transactionInfo.hash)
+                            if (unconfirmedTxList.includes(transaction.transactionInfo.hash)) {
+                                unconfirmedTxList.splice(confirmedTxList.indexOf(transaction.transactionInfo.hash), 1)
+                            }
+                            notice.destroy()
+                            notice.success({
+                                title: this.app.$t('Transaction_Reception').toString(),
+                                duration: 4,
+                            })
+                        }
+                    })
+            })
+
+    }
+
+    txErrorListener(): void {
+        this.txStatusListener && this.txStatusListener.close()
+        const {errorTxList} = this
+        const store = this.app.$store
+        const notice = this.app.$Notice
+        this.txStatusListener = new Listener(this.node, WebSocket)
+        this.txStatusListener
+            .open()
+            .then(() => {
+                this.txStatusListener
+                    .status(Address.createFromRawAddress(this.address))
+                    .subscribe(transaction => {
+                        if (!errorTxList.includes(transaction.hash)) {
+                            errorTxList.push(transaction.hash)
+                            store.commit('SET_ERROR_TEXT', errorTxList)
+                            notice.destroy()
+                            notice.error({
+                                title: transaction.status.split('_').join(' '),
+                                duration: 10,
+                            })
+                        }
+                    })
+            })
+
+    }
+
+    chainListener() {
+        console.log('chainListener called', this, this.newBlocksListener)
+        const store = this.app.$store
+        this.newBlocksListener && this.newBlocksListener.close()
+        this.newBlocksListener = new Listener(this.node, WebSocket)
+        this.newBlocksListener
+            .open()
+            .then(() => {
+                this.newBlocksListener
+                    .newBlock()
+                    .subscribe(block => {
+                        store.commit('SET_CHAIN_STATUS', {
+                            numTransactions: block.numTransactions ? block.numTransactions : 0,
+                            signerPublicKey: block.signer.publicKey,
+                            currentHeight: block.height.compact(),
+                            currentBlockInfo: block,
+                        })
+                    })
+            })
+    }
 }
